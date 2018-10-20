ee14138a50a85ab50833372eba4007ca0566fe53
SeanErvinson <ervinsonong@gmail.com>
2018-10-21 01:20:14 +0800

    Transferred Numbers to separate package and renamed it to better represent.

---
+++ app/src/main/java/com/seanervinson/numberstowords/NumberUtilities/EnglishNumerals.java
@@ -0,0 +1,9 @@
+package com.seanervinson.numberstowords.NumberUtilities;
+
+public class EnglishNumerals {
+    public static final String[] TENS = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    public static final String[] TYS = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    public static final String[] LARGE_NUMBERS = {"Thousand", "Million", "Billion", "Trillion",
+            "Quadrillion", "Quintillion", "Sextillion", "Septillion", "Octillion", "Nonillion"};
+}
