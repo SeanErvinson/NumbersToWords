72bb3292214ec523c2b8c952643fddd943ddecc2
SeanErvinson <ervinsonong@gmail.com>
2018-10-21 23:15:55 +0800

    Created cheque class to format content to cheque format

---
+++ app/src/main/java/com/seanervinson/numberstowords/NumberUtilities/Cheque.java
@@ -0,0 +1,15 @@
+package com.seanervinson.numberstowords.NumberUtilities;
+
+public class Cheque {
+    public static String toChequeFormat(String content) {
+        StringBuilder sb = new StringBuilder();
+        String[] words = content.trim().split(" ");
+        for (int i = 0; i < words.length; i++) {
+            sb.append(words[i]);
+            if(words[i].equals("Hundred") && i != words.length-1){
+                sb.append("And");
+            }
+        }
+        return sb.append("Only").toString();
+    }
+}
