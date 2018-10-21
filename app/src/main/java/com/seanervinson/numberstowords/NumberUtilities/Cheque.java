05cecd5511f204350de84210306c5788159d54db
SeanErvinson <ervinsonong@gmail.com>
2018-10-21 23:31:01 +0800

    Added spacing for better visual

--- app/src/main/java/com/seanervinson/numberstowords/NumberUtilities/Cheque.java
+++ app/src/main/java/com/seanervinson/numberstowords/NumberUtilities/Cheque.java
@@ -5,9 +5,9 @@ public class Cheque {
         StringBuilder sb = new StringBuilder();
         String[] words = content.trim().split(" ");
         for (int i = 0; i < words.length; i++) {
-            sb.append(words[i]);
-            if(words[i].equals("Hundred") && i != words.length-1){
-                sb.append("And");
+            sb.append(words[i]).append(" ");
+            if (words[i].equals("Hundred") && i != words.length - 1) {
+                sb.append("And ");
             }
         }
         return sb.append("Only").toString();
